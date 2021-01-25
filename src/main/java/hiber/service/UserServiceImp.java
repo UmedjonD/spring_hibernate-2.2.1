7be36bd5cc7100cd760e9f8e08b07f1d80f7d00e
Umedjon <Lazerr_94@mail.ru>
2021-01-25 20:23:56 +0300

    update

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -26,6 +26,11 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> getUserCar(String model, int series) {
+      return userDao.getUserCar(model, series);
+   }
 
 
 }
